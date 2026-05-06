4bf2e92c31d694737493327635ecd8c66306a82c
daudikombe <daudikombe77@gmail.com>
2026-05-06 11:34:13 +0300

    react component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import blogData from "../data/blog";
+import react from 'react';
+import ArticleList from './components/ArticleList';
+import Header from './components/Header';
+import About from "./components/About";
+import Article from "./components/Article"
+import { buildErrorMessage } from 'vite';
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+        <Header name ={blogData.name} />
+        <About about={blogData.about} />
+        <ArticleList posts={blogData.posts} />
     </div>
   );
 }
